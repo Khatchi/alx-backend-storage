253aef4b5f21eac9007d42c495cf70f721d6f753
Khatchi <solomon9ice2014@gmail.com>
2024-01-20 18:42:57 +0100

    Task4:  creates a trigger that decreases the quantity of an item after adding a new order

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,6 @@
+--  A script that creates a trigger that decreases the qty
+CREATE TRIGGER dec_q 
+AFTER INSERT ON orders 
+FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number 
+WHERE name=NEW.item_name;
